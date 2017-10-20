8600e9464f8dce53e7d5b64de90a00d8fcc6bdeb
blhowell <bhowell@aggienetwork.com>
2017-10-20 03:31:17 -0500

    Changes to geo.py

--- geo.py
+++ geo.py
@@ -1,72 +1,60 @@
 #!/usr/bin/python3
 """
 Ideally command line API interaction
+Do not over user this.
+
+
 
 Example Usage:
-geo 8.8.8.8 203.0.113.1
+geo 8.8.8.8 8.8.4.4
 """
 from argparse import ArgumentParser
 import re
 import requests
 
-
-# TODO: IPV6 Support
 # TODO: Add an all command argument flag that will show as much as possible.
-
 
 def geo():
     URL = "http://ip-api.com/json/"
 
     parser = ArgumentParser(description='Return Geo IP Data.')
     # add optional argument of ipv4 ip address
-    parser.add_argument('IP', metavar="IP", type=str, help="An IPv4 Address", nargs='*')
-    args = parser.parse_args()
+    parser.add_argument('IP', metavar="IP", type=str, help="IPv4 Addresses separated by a space", nargs='*')
 
-    args = args.IP
-    print(len(args))
-    print(type(args))
-    print(args)
+    # get arguments. have args become the IP arguments
+    args = parser.parse_args().IP
 
-    if len(args)== 0:
-        print("""Run "geo -h" for help. """)
+    # no args. get user info.
+    if not args:
+        print("NOTE: Run geo with -h for help")
+        request = requests.get(URL)
+        if request.status_code == 200:
+            data = request.json()
+            printer(data)
 
-    for single_ip in args:
-        # # debug
-        # print(single_ip)
-        if is_ip(single_ip):
+    # has args
+    elif args:
+        for single_ip in args:
             request = requests.get(URL + single_ip)
             if request.status_code == 200:
                 data = request.json()
-                if data['status'] == "success":
-                    # print("__________")
-                    print("----------")
-                    if data['country'] != '':
-                        print("| IP:         {0[query]}\n| Country:    {0[country]} ({0[countryCode]})".format(data))
-                    if data['region'] != '':
-                        print("| Region:     {0[regionName]} ({0[region]})".format(data))
-                    if data['zip'] != '':
-                        print("| ZIP:        {0[zip]}".format(data))
-                    if data['isp'] != '':
-                        print("| ISP:        {0[isp]}".format(data))
-                elif data['status'] == "fail":
-                    print("__________")
-                    print("| IP:         {0[query]}".format(data))
-                    print("| Status:     {0[status]}".format(data))
-                    print("| Message:    {0[message]}".format(data))
-            else:
-                print("Bad Request. HTTP Status Code: {.status_code}".format(request))
-        else:
-            # Completely invalid ips. Like... a.b.c.d
-            print("__________")
-            print("| IP:         " + single_ip)
-            print("| Status:     " + "Invalid")
-    print("----------")
+                printer(data)
 
-
-def is_ip(string):
-    # TODO: improve this regex for IPv4 addresses. Easily google-able. or just rely on the website?
-    # regex should be used to get IP that site can handle
-    if re.match('(\d{1,3}.){3}\d{1,3}', string):
-        return string
+def printer(data):
+    if data['status'] == "success":
+        print("----------")
+        if data['country'] != '':
+            print("| IP:         {0[query]}\n| Country:    {0[country]} ({0[countryCode]})".format(data))
+        if data['region'] != '':
+            print("| Region:     {0[regionName]} ({0[region]})".format(data))
+        if data['zip'] != '':
+            print("| ZIP:        {0[zip]}".format(data))
+        if data['isp'] != '':
+            print("| ISP:        {0[isp]}".format(data))
+    elif data['status'] == "fail":
+        print("__________")
+        print("| IP:         {0[query]}".format(data))
+        print("| Status:     {0[status]}".format(data))
+        print("| Message:    {0[message]}".format(data))
 
 geo()
