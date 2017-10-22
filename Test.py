09f0ca295756900979cfc646bef3735a89be9809
blhowell <blhowell@aggienetwork.com>
2017-10-22 18:44:13 -0500

    pep8 changes. Have inputted ip args go through valid_ip(). Changes to example ip list.

---
+++ Test.py
@@ -0,0 +1,4 @@
+import requests
+print("TEST THINGS HERE")
+
+print(requests.get("http://fuckaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.com"))
--- geo.py
+++ geo.py
@@ -13,15 +13,18 @@ from argparse import ArgumentParser
 import re
 import requests
 
-# TODO: Add an all command argument flag that will show as much as possible.
 
 def geo():
-    URL = "http://ip-api.com/json/"
+    """main program"""
+
+    url = "http://ip-api.com/json/"
 
     parser = ArgumentParser(description="Return Geo IP Data.")
     # add optional argument of ipv4 ip address
-    parser.add_argument("IP", metavar = "IP", type = str, help = "IPv4 Addresses separated by a space", nargs = "*")
-    parser.add_argument("-f", "--file", help = ".txt file with IPv4 Addresses separated by newlines")
+    parser.add_argument("IP", metavar="IP", type=str,
+                        help="IPv4 Addresses separated by a space", nargs="*")
+    parser.add_argument(
+        "-f", "--file", help=".txt file with IPv4 Addresses separated by newlines")
 
     # get arguments. have args become the IP arguments
     args = parser.parse_args().IP
@@ -32,7 +35,7 @@ def geo():
     # no args. get user info.
     if not args and not file_args:
         print("NOTE: Run geo with -h for help")
-        request = requests.get(URL)
+        request = requests.get(url)
         if request.status_code == 200:
             data = request.json()
             printer(data)
@@ -40,28 +43,34 @@ def geo():
     # has args
     elif args:
         for single_ip in args:
-            request = requests.get(URL + single_ip)
-            if request.status_code == 200:
-                data = request.json()
-                printer(data)
-
-    # has file args
-    elif file_args:
-        with open(file_args) as file:
-            ips = re.findall("\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", file.read())
-            ips = [ip for ip in ips if valid_ip(ip)]
-
-            for single_ip in ips:
-                request = requests.get(URL + single_ip)
+            if valid_ip(single_ip):
+                request = requests.get(url + single_ip)
                 if request.status_code == 200:
                     data = request.json()
                     printer(data)
 
+    # has file args
+    elif file_args:
+        with open(file_args) as file:
+            ips = re.findall(
+                r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", file.read())
+            ips = [ip for ip in ips if valid_ip(ip)]
+
+            for single_ip in ips:
+                request = requests.get(url + single_ip)
+                if request.status_code == 200:
+                    data = request.json()
+                    printer(data)
+
+
 def printer(data):
+    """Displays information from json pulled from site"""
     if data["status"] == "success":
         print("----------")
         if data["country"] != "":
-            print("| IP:         {0[query]}\n| Country:    {0[country]} ({0[countryCode]})".format(data))
+            print(
+                "| IP:         {0[query]}\n"
+                "| Country:    {0[country]} ({0[countryCode]})".format(data))
         if data["region"] != "":
             print("| Region:     {0[regionName]} ({0[region]})".format(data))
         if data["zip"] != "":
@@ -76,12 +85,14 @@ def printer(data):
 
 
 def valid_ip(address):
+    """Check for IP validity with regex"""
     try:
         host_bytes = address.split('.')
         valid = [int(b) for b in host_bytes]
-        valid = [b for b in valid if b >= 0 and b<=255]
+        valid = [b for b in valid if b >= 0 and b <= 255]
         return len(host_bytes) == 4 and len(valid) == 4
-    except:
+    except ValueError:
         return False
+
 
 geo()
