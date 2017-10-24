f860bba8a63d02c4c0262ce29e6f8558b8207af8
Jake Meyer <jakewmeyer@gmail.com>
2017-10-23 19:30:41 -0500

    Add weather command

---
+++ weather.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+Fetches current weather based on IP location
+
+
+Example Usage:
+weather
+"""
+
+import requests
+
+
+def weather():
+    """main program"""
+
+    # Fetch IP for city data
+    url = "http://ip-api.com/json"
+    request = requests.get(url)
+    if request.status_code == 200:
+        data = request.json()
+        location = data['city']
+
+    apixu_key = "6510b92495fd472ca30155709172803&q"
+    api_url = f"https://api.apixu.com/v1/current.json?key={apixu_key}={location}"
+
+    # Feed city for weather info
+    if data["status"] == "success":
+        request = requests.get(api_url)
+        if request.status_code == 200:
+            data = request.json()
+            printer(data)
+    elif data["status"] == "fail":
+        print("City not found")
+        exit(1)
+
+
+def printer(data):
+    """Displays information from json pulled from site"""
+
+    print("-----------------------")
+    print(f"City: {data['location']['name']}")
+    print(f"Temp: {data['current']['temp_f']}°F")
+    print(f"Feels Like: {data['current']['feelslike_f']}°F")
+    print(f"Humidity: {data['current']['humidity']}%")
+    print(f"Wind speed: {data['current']['wind_mph']} mph")
+    print(f"Condition: {data['current']['condition']['text']}")
+    print("-----------------------")
+
+
+weather()
