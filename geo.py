55bdb9a830290de8b27e2e150933104925d1b30b
blhowell <blhowell@aggienetwork.com>
2017-10-24 20:07:00 -0500

    implement location_dict to store unique IPs by country. Should lead to implmentation of graph creation.

--- geo.py
+++ geo.py
@@ -13,11 +13,31 @@ from argparse import ArgumentParser
 import re
 import requests
 
+# TODO: handle the api limit or find another api that has more uses per minute.
 
 def geo():
     """main program"""
 
     url = "http://ip-api.com/json/"
+    # empty dict. Save data as a dict with a set of IPs. ex: {country: {IP, IP, IP}}
+    location_dict = {}
+
+    """
+    This will likely be moved to a notes repository.
+    NOTE: Dictionary manipulation
+    add new country with empty list as value. (list will allow duplicates.)
+    location_data.update({country:[]})
+    add new country with empty list as value. (set will not have duplicates.)
+    location_data.update({country:set()})
+
+    Check if something in dict:
+    "a" in location_data (returns T or F)
+    country in data
+
+    Add stuff to set:
+    location_data['a'].add("1")
+    location_data[country].add(single_ip)
+    """
 
     parser = ArgumentParser(description="Return Geo IP Data.")
     # add optional argument of ipv4 ip address
@@ -55,12 +75,45 @@ def geo():
             ips = re.findall(
                 r"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}", file.read())
             ips = [ip for ip in ips if valid_ip(ip)]
+            
+            # Enforce Unique! if unique desired. Add flag later? Assume unique by default? Add flag for not uniqueness? Find whomst'd've is most active ip in list?
+            # if unique ips desired:
+            ips = set(ips)
+            
+        for single_ip in ips:
+            request = requests.get(url + single_ip)
+            if request.status_code == 200:
+                data = request.json()
+                printer_and_collector(data, location_dict)
 
-            for single_ip in ips:
-                request = requests.get(url + single_ip)
-                if request.status_code == 200:
-                    data = request.json()
-                    printer(data)
+
+def printer_and_collector(data, location_dict):
+    """Displays information from json pulled from site"""
+    if data["status"] == "success":
+        print("----------")
+        if data["country"] != "":
+            print(
+                "| IP:         {0[query]}\n"
+                "| Country:    {0[country]} ({0[countryCode]})".format(data))
+            
+            # add country and begin set for IPs if the country hasn't shown up yet.
+            if data["country"] not in location_dict:
+                location_dict.update({data["country"]:set()})
+            
+            # add ip to set that is the value to the country key in the dict of countries. 
+            location_dict[data["country"]].add(data["query"])
+
+        if data["region"] != "":
+            print("| Region:     {0[regionName]} ({0[region]})".format(data))
+        if data["zip"] != "":
+            print("| ZIP:        {0[zip]}".format(data))
+        if data["isp"] != "":
+            print("| ISP:        {0[isp]}".format(data))
+    elif data["status"] == "fail":
+        print("__________")
+        print("| IP:         {0[query]}".format(data))
+        print("| Status:     {0[status]}".format(data))
+        print("| Message:    {0[message]}".format(data))
 
 
 def printer(data):
